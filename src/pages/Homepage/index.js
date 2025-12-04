281762f49ce4abc368a1b5ab982376d374526376
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-12-04 22:05:03 +0100

    Change font to Poppins, add background color to body

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -3,8 +3,10 @@ import { MovieList } from "../../components/MovieList";
 export const HomePage = () => {
   return (
     <div>
-      <h1>Popular Movies</h1> //Dodać css
+      <h1>Popular Movies</h1>
       <MovieList />
     </div>
   );
 };
+
+export default HomePage;
