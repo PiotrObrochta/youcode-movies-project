deda8cf9ff3b356bd7202fdcea39dbd992f75edf
Pawel Kruczek <pkruczek00@gmail.com>
2025-12-07 14:18:54 +0100

    add redux to project (store, moviesSlice)

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 import { GlobalStyle } from "./GlobalStyle";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store} >
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
