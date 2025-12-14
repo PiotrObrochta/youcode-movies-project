616119497b666a8e28256eb9c9386a9629ce0048
Pawel Kruczek <pkruczek00@gmail.com>
2025-12-14 16:08:04 +0100

    add media queries for mobile view

--- src/common/ErrorView/styled.js
+++ src/common/ErrorView/styled.js
@@ -8,12 +8,21 @@ export const Container = styled.div`
     grid-template-columns: 1fr;
     grid-gap: 24px;
     justify-items: center;
-    margin: 180px auto;   
+    margin: 180px auto;
+    
+    @media (max-width: 767px) {
+        margin-top: 90px
+    }
 `
 
 export const DangerIcon = styled(Danger)`
     width: 120px;
     height: 120px;
+
+    @media (max-width: 767px) {
+        width: 80px;
+        height: 80px;
+    }
 `
 
 export const Header = styled.h1`
@@ -22,6 +31,11 @@ export const Header = styled.h1`
     line-height: 120%;
     text-align: center;
     margin: 0px;
+
+    @media (max-width: 767px) {
+        font-size: 14px;
+        font-weight: 500;
+    }
 `
 
 export const Info = styled.p`
@@ -30,11 +44,16 @@ export const Info = styled.p`
     line-height: 130%;
     text-align: center;
     margin: 0px;
+
+    @media (max-width: 767px) {
+        font-size: 10px;
+        font-weight: 400;
+    }
 `
 
 export const HomepageButton = styled.button`
     padding: 16px 24px;
-    height: 51px;
+    height: auto;
     font-size: 14px;
     font-weight: 700;
     line-height: 19px;
@@ -49,5 +68,11 @@ export const HomepageButton = styled.button`
 
     &:active {
         background-color: hsla(220, 100%, 60%, 1.00);
+    }
+
+    @media (max-width: 767px) {
+        font-size: 10px;
+        font-weight: 400;
+        padding: 8px 12px;
     }
 `
