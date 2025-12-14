167b63599303f367ed0092c98cf202b44d8de77e
Kamila Falgowska <kamilafalgowska@gmail.com>
2025-12-14 17:37:36 +0100

    add HashRouter, Switch, Route, Redirect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import Navigation from "./common/Navigation";
 import MoviesPage from "./features/movies/moviesPage";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Navigation />
-      <MoviesPage />
-    </>
+      <Switch>
+        <Route path="/homepage">
+          <MoviesPage />
+        </Route>
+        <Route path="/">
+          <Redirect to="/homepage" />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
