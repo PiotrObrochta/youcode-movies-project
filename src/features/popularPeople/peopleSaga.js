61e25bc2c0589354aa03e61d0418e5d340e15e5d
Piotr Obrochta <piotr.obrochta88@gmial.com>
2025-12-19 11:50:48 +0100

    fix(popularPeople): correct saga for fetching people

--- src/features/popularPeople/peopleSaga.js
+++ src/features/popularPeople/peopleSaga.js
@@ -1,22 +1,25 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
-import { fetchPopularPeople, setFetchPopularPeopleStatus, setPopularPeople } from "./peopleSlice"
+import {
+  fetchPopularPeople,
+  setFetchPopularPeopleStatus,
+  setPopularPeople,
+} from "./peopleSlice";
 import { getDataFromApi } from "../tmdbApi";
 
-function* fetchPopularPeopleHandler (page) {
-    try {
-        yield put(setFetchPopularPeopleStatus('loading'));
-        yield delay(1000);
+function* fetchPopularPeopleHandler({ payload: page }) {
+  try {
+    yield put(setFetchPopularPeopleStatus("loading"));
+    yield delay(1000);
 
-        const People = yield call(getDataFromApi, "people", page || 1);
+    const People = yield call(getDataFromApi, "people", page || 1);
 
-        yield put(setPopularPeople(People))
-        yield put(setFetchPopularPeopleStatus("success"))
-    } catch (error) {
-        yield put(setFetchPopularPeopleStatus("error"))
-    }
+    yield put(setPopularPeople(People));
+    yield put(setFetchPopularPeopleStatus("success"));
+  } catch (error) {
+    yield put(setFetchPopularPeopleStatus("error"));
+  }
 }
 
-export default function* peopleSaga () {
-    yield call(fetchPopularPeopleHandler)
-    yield takeLatest(fetchPopularPeople.type, fetchPopularPeopleHandler);
+export default function* peopleSaga() {
+  yield takeLatest(fetchPopularPeople.type, fetchPopularPeopleHandler);
 }
